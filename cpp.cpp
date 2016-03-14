68e379c5c8dd1a9e420075dc4d3bce12105af6d6
Dmitry Ponyatov <dponyatov@gmail.com>
2016-03-14 12:52:12 +0400

    + ext:fileio + {lam: bda} /^@apply not works yet/ + bI skelex
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cpp.cpp
+++ cpp.cpp
@@ -3,16 +3,19 @@
 void yyerror(string msg) { cout<<YYERR; cerr<<YYERR; exit(-1); }
 int main() { env_init(); return yyparse(); }
 
+// abstract symbolic type
+
 Sym::Sym(string T,string V) { tag=T; val=V; }
 Sym::Sym(string V):Sym("",V) {}
 void Sym::push(Sym*o) { nest.push_back(o); }
+void Sym::par(Sym*o) { pars[o->val]=o; }
 
 string Sym::tagval() { return "<"+tag+":"+val+">"; }
 string Sym::tagstr() { return "<"+tag+":'"+val+"'>"; }
 string Sym::pad(int n) { string S; for (int i=0;i<n;i++) S+='\t'; return S; }
 string Sym::dump(int depth) { string S = "\n"+pad(depth)+tagval();
-//	for (auto pr=pars.begin(),e=pars.end();pr!=e;pr++)
-//		S += ","+pr->first;
+	for (auto pr=pars.begin(),e=pars.end();pr!=e;pr++)
+		S += ","+pr->first;
 	for (auto it=nest.begin(),e=nest.end();it!=e;it++)
 		S += (*it)->dump(depth+1);
 	return S; }
@@ -24,6 +27,12 @@ Sym* Sym::eval() {
 	return this; }
 
 Sym* Sym::eq(Sym*o) { env[val]=o; return o; }
+Sym* Sym::at(Sym*o) { push(o); return this; }
+
+Sym* Sym::add(Sym*o) { Sym*R = new Op("+");
+	R->push(this); R->push(o); return R; }
+
+// scalars
 
 Str::Str(string V):Sym("str",V) {}
 Sym* Str::eval() { return this; }
@@ -44,16 +53,41 @@ Sym* Hex::eval() { return this; }
 Bin::Bin(string V):Sym("bin",V) {}
 Sym* Bin::eval() { return this; }
 
+// composites
+
 List::List():Sym("[","]") {}
 Vector::Vector():Sym("<",">") {}
 Cons::Cons(Sym*A,Sym*B):Sym("","") { push(A); push(B); }
 
+// functionals
+
 Op::Op(string V):Sym("op",V) {}
-Sym* Op::eval() { Sym::eval();
-	if (val=="=") return nest[0]->eq(nest[1]);
+Sym* Op::eval() {
+	if (val=="~") return nest[0]; else Sym::eval();	// quote or nest[]ed eval
+	if (val=="=") return nest[0]->eq(nest[1]);		// A = B assign
+	if (val=="@") return nest[0]->at(nest[1]);		// A @ B apply
+	if (val=="+") return nest[0]->add(nest[1]);		// A + B add
 	return this; }
 
+Fn::Fn(string V, FN F):Sym("fn",V) { fn=F; }
+Sym* Fn::at(Sym*o) { return fn(o); }
+
 Lambda::Lambda():Sym("^","^") {}
+Sym* Lambda::eval() { return this; }
+
+// ext:fileio
+
+Dir::Dir(Sym*o):Sym("dir",o->val) {}
+Sym* Dir::dir(Sym*o) { return new Dir(o); }
+Sym* Dir::add(Sym*o) {
+	Sym*F = o;
+	if ((o->tag!="dir")&&(o->tag=="file")) F = new File(o);
+	push(F); return F; }
+
+File::File(Sym*o):Sym("file",o->val) {}
+Sym* File::file(Sym*o) { return new File(o); }
+
+// global environment
 
 map<string,Sym*> env;
 void env_init() {
@@ -64,4 +98,7 @@ void env_init() {
 	env["N"] = new Sym("nil","N");
 	env["D"] = new Sym("default","D");
 	env["E"] = new Sym("error","E");
+	// ext:fileio
+	env["dir"] = new Fn("dir",Dir::dir);
+	env["file"] = new Fn("file",File::file);
 }
--- hpp.hpp
+++ hpp.hpp
@@ -8,20 +8,27 @@
 #include <map>
 using namespace std;
 
+// abstract symbolic type
+
 struct Sym {
 	string tag,val;
 	Sym(string,string); Sym(string);
 	vector<Sym*> nest; void push(Sym*);
-//	map<string,Sym*> pars; void par(Sym*);
+	map<string,Sym*> pars; void par(Sym*);
 	virtual string tagval(); string tagstr();
 	virtual string dump(int=0); string pad(int);
 	virtual Sym* eval();
-	virtual Sym* eq(Sym*);
-//	virtual Sym* at(Sym*);
+	virtual Sym* eq(Sym*);	// A = B assign
+	virtual Sym* at(Sym*);	// A @ B apply
+	virtual Sym* add(Sym*);	// A + B add
 };
+
+// global environment
 
 extern map<string,Sym*> env;
 extern void env_init();
+
+// scalars
 
 struct Str: Sym { Str(string); Sym*eval(); string tagval(); };
 struct Int: Sym { Int(string); Sym*eval(); long val; string tagval(); };
@@ -29,13 +36,23 @@ struct Num: Sym { Num(string); Sym*eval(); double val; string tagval(); };
 struct Hex: Sym { Hex(string); Sym*eval(); };
 struct Bin: Sym { Bin(string); Sym*eval(); };
 
+// composites
+
 struct List: Sym { List(); };
 struct Vector: Sym { Vector(); };
 struct Cons: Sym { Cons(Sym*,Sym*); };
 
-struct Op: Sym { Op(string); Sym*eval(); };
+// functionals
 
-struct Lambda: Sym { Lambda(); };
+struct Op: Sym { Op(string); Sym*eval(); };
+typedef Sym*(*FN)(Sym*);
+struct Fn: Sym { Fn(string,FN); FN fn; Sym*at(Sym*); };
+struct Lambda: Sym { Lambda(); Sym*eval(); };
+
+// ext:fileio
+
+struct Dir: Sym { Dir(Sym*); static Sym* dir(Sym*); Sym*add(Sym*); };
+struct File: Sym { File(Sym*); static Sym* file(Sym*); };
 
 extern int yylex();
 extern int yylineno;
