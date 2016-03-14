80c14cd7804b6891f5f8233d2c06afc67820ad2b
Dmitry Ponyatov <dponyatov@gmail.com>
2016-03-14 12:04:19 +0400

    + scalars [list] <vector> co,ns specials var/assign
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cpp.cpp
+++ cpp.cpp
@@ -13,8 +13,8 @@ string Sym::pad(int n) { string S; for (int i=0;i<n;i++) S+='\t'; return S; }
 string Sym::dump(int depth) { string S = "\n"+pad(depth)+tagval();
 //	for (auto pr=pars.begin(),e=pars.end();pr!=e;pr++)
 //		S += ","+pr->first;
-//	for (auto it=nest.begin(),e=nest.end();it!=e;it++)
-//		S += (*it)->dump(depth+1);
+	for (auto it=nest.begin(),e=nest.end();it!=e;it++)
+		S += (*it)->dump(depth+1);
 	return S; }
 
 Sym* Sym::eval() {
@@ -23,11 +23,45 @@ Sym* Sym::eval() {
 		(*it) = (*it)->eval();
 	return this; }
 
+Sym* Sym::eq(Sym*o) { env[val]=o; return o; }
 
 Str::Str(string V):Sym("str",V) {}
+Sym* Str::eval() { return this; }
 string Str::tagval() { return tagstr(); }
+
+Int::Int(string V):Sym("int",V) { val=atol(V.c_str()); }
+Sym* Int::eval() { return this; }
+string Int::tagval() { ostringstream os; os<<"<"<<tag<<":"<<val<<">";
+	return os.str(); }
+
+Num::Num(string V):Sym("num",V) { val=atof(V.c_str()); }
+Sym* Num::eval() { return this; }
+string Num::tagval() { ostringstream os; os<<"<"<<tag<<":"<<val<<">";
+	return os.str(); }
+
+Hex::Hex(string V):Sym("hex",V) {}
+Sym* Hex::eval() { return this; }
+Bin::Bin(string V):Sym("bin",V) {}
+Sym* Bin::eval() { return this; }
+
+List::List():Sym("[","]") {}
+Vector::Vector():Sym("<",">") {}
+Cons::Cons(Sym*A,Sym*B):Sym("","") { push(A); push(B); }
+
+Op::Op(string V):Sym("op",V) {}
+Sym* Op::eval() { Sym::eval();
+	if (val=="=") return nest[0]->eq(nest[1]);
+	return this; }
+
+Lambda::Lambda():Sym("^","^") {}
 
 map<string,Sym*> env;
 void env_init() {
 	env["MODULE"] = new Sym(MODULE);
+	// specials
+	env["T"] = new Sym("bool","true");
+	env["F"] = new Sym("bool","false");
+	env["N"] = new Sym("nil","N");
+	env["D"] = new Sym("default","D");
+	env["E"] = new Sym("error","E");
 }
--- hpp.hpp
+++ hpp.hpp
@@ -2,6 +2,7 @@
 #define _H_STONED
 
 #include <iostream>
+#include <sstream>
 #include <cstdlib>
 #include <vector>
 #include <map>
@@ -15,14 +16,26 @@ struct Sym {
 	virtual string tagval(); string tagstr();
 	virtual string dump(int=0); string pad(int);
 	virtual Sym* eval();
-//	virtual Sym* eq(Sym*);
+	virtual Sym* eq(Sym*);
 //	virtual Sym* at(Sym*);
 };
 
 extern map<string,Sym*> env;
 extern void env_init();
 
-struct Str: Sym { Str(string); string tagval(); };
+struct Str: Sym { Str(string); Sym*eval(); string tagval(); };
+struct Int: Sym { Int(string); Sym*eval(); long val; string tagval(); };
+struct Num: Sym { Num(string); Sym*eval(); double val; string tagval(); };
+struct Hex: Sym { Hex(string); Sym*eval(); };
+struct Bin: Sym { Bin(string); Sym*eval(); };
+
+struct List: Sym { List(); };
+struct Vector: Sym { Vector(); };
+struct Cons: Sym { Cons(Sym*,Sym*); };
+
+struct Op: Sym { Op(string); Sym*eval(); };
+
+struct Lambda: Sym { Lambda(); };
 
 extern int yylex();
 extern int yylineno;
